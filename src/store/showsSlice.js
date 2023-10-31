7cfb23da5e4e0d07f428e76efe2f518420cade93
MishaHudz <misha.hudz@gmail.com>
2023-10-31 21:10:32 +0200

    bugfixed

--- src/store/showsSlice.js
+++ src/store/showsSlice.js
@@ -19,7 +19,7 @@ const showsSlice = createSlice({
     },
     findShow: (state, { payload }) => {
       state.show = state.showsList?.find(
-        el => el.show.id === Number(payload)
+        el => el?.show.id === Number(payload)
       ).show;
     },
   },
